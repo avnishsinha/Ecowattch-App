b7dcf7c991402cb63531d74d14aea8381af940c4
risa walles <rmw395@nau.edu>
2025-10-08 23:41:48 -0700

    dashboard buttons

---
+++ app/src/main/java/com/example/ecowattchtechdemo/utils/Dashboard.java
@@ -0,0 +1,37 @@
+package com.example.ecowattchtechdemo.utils;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+public class Dashboard extends AppCompatActivity {
+    Button records, shop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        records = (android.widget.LinearLayout)findViewById(R.id.records_button);
+        shop = (android.widget.LinearLayout)findViewById(R.id.shop_button);
+
+        records.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, Records.class);
+                startActivity(intent);
+            }
+        });
+
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, Shop.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/ecowattchtechdemo/utils/Records.java
@@ -0,0 +1,10 @@
+package com.example.ecowattchtechdemo.utils;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+public class Records extends AppCompatActivity {
+}
