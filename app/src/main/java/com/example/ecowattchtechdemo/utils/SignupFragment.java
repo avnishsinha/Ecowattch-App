75e179030c39f14256367c3bd214df592a5e05e5
risa walles <rmw395@nau.edu>
2025-10-09 10:52:36 -0700

    signup/login buttons

--- app/src/main/java/com/example/ecowattchtechdemo/utils/SignupFragment.java
+++ app/src/main/java/com/example/ecowattchtechdemo/utils/SignupFragment.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.ecowattchtechdemo.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignupFragment extends Fragment {
+    Button signupButton;
+    TextView loginLink;
+    TextInputEditText signupUser, signupPass, confirmPass, dorm;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
