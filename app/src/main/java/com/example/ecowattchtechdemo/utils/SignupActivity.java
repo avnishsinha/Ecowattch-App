cb6d69e82dc8e29c5ced97600105e1ffdf44d120
risa walles <rmw395@nau.edu>
2025-10-09 02:44:36 -0700

    signup fragment

--- app/src/main/java/com/example/ecowattchtechdemo/utils/SignupActivity.java
+++ app/src/main/java/com/example/ecowattchtechdemo/utils/SignupActivity.java
@@ -8,5 +8,17 @@ import android.content.Intent;
 import com.example.ecowattchtechdemo.R;
 
 public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
 
+        // Only add the fragment if it’s not already added
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.login_signup_fragment_container, new SignupFragment())
+                    .commit();
+        }
+    }
 }
